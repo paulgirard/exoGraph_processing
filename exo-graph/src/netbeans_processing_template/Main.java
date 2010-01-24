052e15171653371ea75079ef8787e371c938b84a
marge <marge@marge>
2010-01-24 18:07:29 +0100

    initial

---
+++ exo-graph/src/netbeans_processing_template/Main.java
@@ -0,0 +1,29 @@
+// NOTE: You need to manually change the package name on the next line and in the line within the main function.
+// This will be automated in future versions once my Netbeans powers have developed...
+package netbeans_processing_template;
+
+
+import processing.core.PApplet;
+
+/**
+ *
+ */
+public class Main extends PApplet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PApplet.main(new String[]{"netbeans_processing_template.Main"});
+    }
+
+    public void setup() {
+        // TODO: setup application
+        size(400,400);
+        ellipse(200,200,50,50);
+    }
+
+    public void draw() {
+        // TODO: handle each frame of drawing
+    }
+}
